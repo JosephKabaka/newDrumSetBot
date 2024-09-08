6029738bbf720cc743be483f77520b490f916baf
JosephKabaka <129948519+JosephKabaka@users.noreply.github.com>
2024-09-08 13:48:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+
+for(let i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+    { 
+       var buttonInnerHTML=this.innerHTML; 
+        switch (buttonInnerHTML) {
+        case "W":
+           var audio = new Audio('sounds/tom-1.mp3');
+           audio.play();
+        break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+         break;
+         case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+         break;
+         case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+         break;
+         case "j":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+         break;
+         case "k":
+            var audio = new Audio('sounds/jj.mp3');
+            audio.play();
+         break;
+         default :
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+         
+    } 
+});
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//         audio.play();
